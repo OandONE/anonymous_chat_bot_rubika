cb7b4a16c3b1bab213e4928aec755dcc48c54349
01 <159622426+OandONE@users.noreply.github.com>
2025-07-28 22:51:11 +0330

    Update anymamus_bot.py

--- anymamus_bot.py
+++ anymamus_bot.py
@@ -1,5 +1,5 @@
 from fast_rub import Client,Update,filters
-import json,random
+import json,random,httpx
 bot=Client("anymamus_bot")
 def open_file(name_file,type_file="dict"):
     try:
@@ -22,6 +22,9 @@ ids=open_file("ids.json")
 i_send=open_file("i_send.json")
 warns=open_file("warns.json")
 bans=open_file("bans.json","list")
+def chek_fohsh(text:str):
+    with httpx.Client() as cl:
+        return ((cl.get(f"http://api.parssource.ir/fohsh/?text={text}",timeout=30)).json())['result']['is_foshs']
 @bot.on_message_updates(filters=filters.is_user())
 async def main(message:Update):
     TEXT_MESSAGE=message.text
@@ -76,6 +79,21 @@ async def main(message:Update):
                     save_file("bans.json",bans)
                     save_file("warns.json",warns)
                     return None
+            if chek_fohsh(TEXT_MESSAGE):
+                if CHAT_ID in warns:
+                    warns[CHAT_ID]+=1
+                else:
+                    warns[CHAT_ID]=1
+                await message.reply(f"""ارسال لینک/فحش در پیغام مجاز نیست ❌
+اخطار های شما : {warns[CHAT_ID]}/3
+در صورت پر شدن اخطار ها شما از ارسال پیام با ربات محروم میشوید.""")
+                if warns[CHAT_ID]==3:
+                    bans.append(CHAT_ID)
+                    warns[CHAT_ID]=0
+                    await message.reply("شما به دلیل پر شدن اخطار هایتان از ربات بن شدید ❌ پشتیبانی ربات : @O_and_ONE_01")
+                save_file("bans.json",bans)
+                save_file("warns.json",warns)
+                return None
             for g,i in ids.items():
                 if i==i_send[CHAT_ID]:
                     await bot.send_text(f"""✉️ پیام جدید داری :
